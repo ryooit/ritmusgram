1cbc2b31530dce630376c01c53bf7b2e02604afc
INTAE RYOO <ryooit70@gmail.com>
2018-01-02 20:10:59 -0500

    add comment, like class with foreign key

--- ritmusgram/images/models.py
+++ ritmusgram/images/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from ritmusgram.users import models as user_models
 
 # Create your models here.
 class TimeStampedModel(models.Model):
@@ -12,11 +13,26 @@ class TimeStampedModel(models.Model):
 
 class Image(TimeStampedModel):
 
+    """ Image Model """
+
     file = models.ImageField()
     location = models.CharField(max_length=140)
     caption = models.TextField()
+    creator = models.ForeignKey(user_models.User, null=True)
 
 
 class Comment(TimeStampedModel):
 
+    """ Comment Model """
+
     message = models.TextField()
+    creator = models.ForeignKey(user_models.User, null=True)
+    image = models.ForeignKey(Image, null=True)
+
+
+class Like(TimeStampedModel):
+
+    """ Like Model """
+
+    creator = models.ForeignKey(user_models.User, null=True)
+    image = models.ForeignKey(Image, null=True)
