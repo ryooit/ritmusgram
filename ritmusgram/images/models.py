fb2803b8e9df7622bbd2abdd8ee18c4a18c124ee
INTAE RYOO <ryooit70@gmail.com>
2018-01-04 14:40:11 -0500

    customize admin panel

--- ritmusgram/images/models.py
+++ ritmusgram/images/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 from ritmusgram.users import models as user_models
 
 # Create your models here.
+@python_2_unicode_compatible
 class TimeStampedModel(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
@@ -10,7 +12,7 @@ class TimeStampedModel(models.Model):
     class Meta:
         abstract = True
 
-
+@python_2_unicode_compatible
 class Image(TimeStampedModel):
 
     """ Image Model """
@@ -20,7 +22,10 @@ class Image(TimeStampedModel):
     caption = models.TextField()
     creator = models.ForeignKey(user_models.User, null=True)
 
+    def __str__(self):
+        return '{} - {}'.format(self.location, self.caption)
 
+@python_2_unicode_compatible
 class Comment(TimeStampedModel):
 
     """ Comment Model """
@@ -29,10 +34,16 @@ class Comment(TimeStampedModel):
     creator = models.ForeignKey(user_models.User, null=True)
     image = models.ForeignKey(Image, null=True)
 
+    def __str__(self):
+        return self.message
 
+@python_2_unicode_compatible
 class Like(TimeStampedModel):
 
     """ Like Model """
 
     creator = models.ForeignKey(user_models.User, null=True)
     image = models.ForeignKey(Image, null=True)
+
+    def __str__(self):
+        return 'User: {} - Image caption: {}'.format(self.creator.username, self.image.caption)
