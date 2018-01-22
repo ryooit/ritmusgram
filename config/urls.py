2c9b08a968a6b9b64036fec96e875fea297ddfcc
INTAE RYOO <ryooit70@gmail.com>
2018-01-21 22:45:34 -0600

    setup django REST framework, add serializers.py for translating Python to JSON

--- config/urls.py
+++ config/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
 
     # User management
     url(r'^users/', include('ritmusgram.users.urls', namespace='users')),
+    url(r'^images/', include('ritmusgram.images.urls', namespace='images')),
     url(r'^accounts/', include('allauth.urls')),
 
     # Your stuff: custom urls includes go here
