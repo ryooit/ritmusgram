31039e202ff0ca6152abbae0faa68120253657b6
INTAE RYOO <ryooit70@gmail.com>
2018-01-29 06:39:16 -0600

    add images and implement like and unlike images

--- ritmusgram/images/views.py
+++ ritmusgram/images/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from . import models, serializers
 
 
@@ -33,4 +34,28 @@ class LikeImage(APIView):
 
     def get(self, request, image_id, format=None):
 
-        return Response(status=200)
+        user = request.user
+
+        try:
+            found_image = models.Image.objects.get(id=image_id)
+        except models.Image.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        try:
+            preexisting_like = models.Like.objects.get(
+                creator = user,
+                image = found_image
+            )
+            preexisting_like.delete()
+
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        except models.Like.DoesNotExist:
+
+            new_like = models.Like.objects.create(
+                creator = user,
+                image = found_image
+            )
+            new_like.save()
+
+            return Response(status=status.HTTP_201_CREATED)
