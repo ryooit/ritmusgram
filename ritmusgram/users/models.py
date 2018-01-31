c8efd2c600d9f4049ea104a9bfc6d845182a1b89
INTAE RYOO <ryooit70@gmail.com>
2018-01-31 11:06:35 -0600

    get follower and following list

--- ritmusgram/users/models.py
+++ ritmusgram/users/models.py
@@ -18,9 +18,9 @@ class User(AbstractUser):
 
     # First Name and Last Name do not cover name patterns
     # around the globe.
-    profile_image = models.ImageField(null=True)
+    profile_image = models.ImageField(null=True, blank=True)
     name = models.CharField(_('Name of User'), blank=True, max_length=255)
-    website = models.URLField(null=True)
+    website = models.URLField(null=True, blank=True)
     bio = models.TextField(null=True)
     phone = models.CharField(max_length=140, null=True)
     gender = models.CharField(max_length=80, choices=GENDER_CHOICES, null=True)
