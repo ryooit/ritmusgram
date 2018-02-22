704f759b15e6459a4a099805c7b5c3b08625b7fe
INTAE RYOO <ryooit70@gmail.com>
2018-02-22 11:41:45 -0600

    add reactotron, redux dev tools, and i18n

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -2,7 +2,10 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { routerReducer, routerMiddleware } from "react-router-redux";
 import createHistory from "history/createBrowserHistory";
+import { composeWithDevTools } from "redux-devtools-extension";
 import users from 'redux/modules/users';
+import { i18nState } from "redux-i18n";
+import Reactotron from "ReactotronConfig";
 
 const env = process.env.NODE_ENV;
 
@@ -15,14 +18,23 @@ if(env === "development"){
   middlewares.push(logger);
 }
 
-
 const reducer = combineReducers({
   users,
-  routing: routerReducer
+  routing: routerReducer,
+  i18nState
 });
 
-let store = initialState =>
-  createStore(reducer, applyMiddleware(...middlewares));
+let store;
+if(env === "development"){
+  store = initialState =>
+    Reactotron.createStore(
+      reducer,
+      composeWithDevTools(applyMiddleware(...middlewares))
+    );
+} else {
+  store = initialState =>
+    createStore(reducer, applyMiddleware(...middlewares));
+}
 
 export { history };
 
