1e871c4d4f3ec9457da315b4e3ded26948c1c601
INTAE RYOO <ryooit70@gmail.com>
2018-02-27 22:11:53 -0600

    add Auth componets

---
+++ frontend/src/components/Auth/index.js
@@ -0,0 +1,11 @@
+import { connect } from "react-redux";
+import Container from "./container";
+
+// Add all the action for:
+// Log In
+// Sign Up
+// Recover password
+// Check
+// Check password
+
+export default connect()(Container);
