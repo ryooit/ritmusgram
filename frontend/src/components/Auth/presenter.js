3bc9f0cc054739b4c296c8d3b8c369b2e336064c
INTAE RYOO <ryooit70@gmail.com>
2018-02-28 12:26:54 -0600

    add translation funtionality

--- frontend/src/components/Auth/presenter.js
+++ frontend/src/components/Auth/presenter.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styles from "./styles.scss"
 import { LoginForm, SignupForm } from "components/AuthForms";
 
@@ -20,29 +21,29 @@ const Auth = (props, context) => (
       <div className={styles.whiteBox}>
         {props.action === "login" && (
           <p>
-            Don&apos;t have an account?{" "}
+            {context.t("Don't have an acoount?")}{" "}
             <span
               onClick={props.changeAction}
               className={styles.changeLink}
               >
-              Sign up
+              {context.t("Sign Up")}
             </span>
           </p>
         )}
         {props.action === "signup" && (
           <p>
-            Have an account?{" "}
+            {context.t("Have an account?")}{" "}
             <span
               onClick={props.changeAction}
               className={styles.changeLink}
             >
-              Log in
+              {context.t("Log in")}
             </span>
           </p>
         )}
       </div>
       <div className={styles.appBox}>
-        <span>Get the app</span>
+        <span>{context.t("Get the app")}</span>
         <div className={styles.appstores}>
           <img
             src={require("images/ios.png")}
@@ -55,5 +56,9 @@ const Auth = (props, context) => (
     </div>
   </main>
 );
+
+Auth.contextTypes = {
+  t: PropTypes.func.isRequired
+}
 
 export default Auth;
--- frontend/src/translations.js
+++ frontend/src/translations.js
@@ -1,5 +1,30 @@
 export const translations = {
-  es: {
-    "Login": "Hola"
+  kr: {
+    "About Us": "INSTAGRAM 정보",
+    "Support": "지원",
+    "Blog": "블로그",
+    "Press": "홍보센터",
+    "API": "API",
+    "Jobs": "채용 정보",
+    "Privacy": "개인정보처리방침",
+    "Terms": "약관",
+    "Directory": "디렉터리",
+    "Language": "언어",
+    "Username": "사용자 이름",
+    "Password": "비밀번호",
+    "Log in": "로그인",
+    "or": "또는",
+    "Log in with Facebook": "Facebook으로 로그인",
+    "Forgot password?": "비밀번호를 잊으셨나요?",
+    "Sign up to see photos and videos from your friends.": "친구들의 사진과 동영상을 보려면 가입하세요.",
+    "Email": "이메일 주소",
+    "Full Name": "성명",
+    "Sign up": "가입",
+    "By signing up, you agree to our Terms & Privacy Policy.": "가입하면 Instagram의 약관 및 개인정보처리방침에 동의하게 됩니다.",
+    "Don't have an acoount?": "계정이 없으신가요?",
+    "Sign Up": "가입하기",
+    "Get the app": "앱을 다운로드하세요.",
+    "Have an account?": "계정이 있으신가요?",
+
   }
 }
