7a99466a50a9f72e899fbe423fa95f0d2e66f553
INTAE RYOO <ryooit70@gmail.com>
2018-02-28 19:47:20 -0600

    control input and submit on SignupForm

--- frontend/src/components/SignupForm/presenter.js
+++ frontend/src/components/SignupForm/presenter.js
@@ -11,26 +11,60 @@ const SignupForm = (props, context) => (
       {context.t("Log in with Facebook")}
     </button>
     <span className={formStyles.divider}>{context.t("or")}</span>
-    <form className={formStyles.form}>
-      <input type="email" placeholder={context.t("Email")} className={formStyles.textInput} />
-      <input type="text" placeholder={context.t("Full Name")} className={formStyles.textInput} />
+    <form className={formStyles.form} onSubmit={props.handleSubmit}>
+      <input
+        type="email"
+        placeholder={context.t("Email")}
+        className={formStyles.textInput}
+        value={props.emailValue}
+        onChange={props.handleInputChange}
+        name="email"
+      />
+      <input
+        type="text"
+        placeholder={context.t("Full Name")}
+        className={formStyles.textInput}
+        value={props.fullnameValue}
+        onChange={props.handleInputChange}
+        name="fullname"
+      />
       <input
         type="username"
         placeholder={context.t("Username")}
         className={formStyles.textInput}
+        value={props.usernameValue}
+        onChange={props.handleInputChange}
+        name="username"
       />
       <input
         type="password"
         placeholder={context.t("Password")}
         className={formStyles.textInput}
+        value={props.passwordValue}
+        onChange={props.handleInputChange}
+        name="password"
       />
-      <input type="submit" value={context.t("Sign up")} className={formStyles.button} />
+      <input
+        type="submit"
+        value={context.t("Sign up")}
+        className={formStyles.button}
+        onChange={props.handleInputChange}
+      />
     </form>
     <p>
       {context.t("By signing up, you agree to our Terms & Privacy Policy.")}
     </p>
   </div>
 );
+
+SignupForm.propTypes = {
+  emailValue: PropTypes.string.isRequired,
+  fullnameValue: PropTypes.string.isRequired,
+  usernameValue: PropTypes.string.isRequired,
+  passwordValue: PropTypes.string.isReuqired,
+  handleInputChange: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired
+}
 
 SignupForm.contextTypes = {
   t: PropTypes.func.isRequired
